5184016273f7cea7daa0cd99183be0844b05cdb0
Slaawwa <slaawwa@gmail.com>
2018-08-02 19:32:27 +0300

    Add tasks script and server

--- src/likeBtn.js
+++ src/likeBtn.js
@@ -1,7 +1,32 @@
 
-        console.log('DONE')
-        // return (
-        //     <button onClick={() => this.setState({ liked: true })}>
-        //         Like
-        //     </button>
-        // );
+import Person from './likeBtn0'
+
+// function Person(props) {
+//     return (
+//         <div className="person">
+//             <h1>{props.name}</h1>
+//             <p>Age: {props.age}</p>
+//         </div>
+//     )
+// }
+
+        console.log('Person:', Person)
+// export default Person
+
+// // ReactDOM.render(<Person name="Slaawwa" age="26" />, document.querySelector('#p1'))
+
+const app = (
+    <div>
+        <Person name="Slaawwa" age="26" />
+        <Person name="Vika" age="25" />
+    </div>
+)
+
+ReactDOM.render(app, document.querySelector('#app'))
+
+//         console.log('DONE')
+//         // return (
+//         //     <button onClick={() => this.setState({ liked: true })}>
+//         //         Like
+//         //     </button>
+//         // );
---
+++ src/likeBtn0.js
@@ -0,0 +1,31 @@
+
+
+function Person(props) {
+    return (
+        <div className="person">
+            <h1>{props.name}</h1>
+            <p>Age: {props.age}</p>
+        </div>
+    )
+}
+
+        // console.log('Person:', Person)
+export default Person
+
+// // ReactDOM.render(<Person name="Slaawwa" age="26" />, document.querySelector('#p1'))
+
+// const app = (
+//     <div>
+//         <Person name="Slaawwa" age="26" />
+//         <Person name="Vika" age="25" />
+//     </div>
+// )
+
+// ReactDOM.render(app, document.querySelector('#app'))
+
+//         console.log('DONE')
+//         // return (
+//         //     <button onClick={() => this.setState({ liked: true })}>
+//         //         Like
+//         //     </button>
+//         // );
