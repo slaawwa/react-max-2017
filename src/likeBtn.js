58c4ee2bdc115f02108534a9d052191b06d8bca5
Slaawwa <slaawwa@gmail.com>
2018-08-02 03:57:04 +0300

    Init commit

---
+++ src/likeBtn.js
@@ -0,0 +1,7 @@
+
+        console.log('DONE')
+        // return (
+        //     <button onClick={() => this.setState({ liked: true })}>
+        //         Like
+        //     </button>
+        // );
